2a21646a525b895e6f98326fbfff2c9e37efccc3
Mohamed Sherif <145474950+mohammed-sherif34@users.noreply.github.com>
2024-08-13 17:20:31 +0300

    finish home screen ui

---
+++ lib/home/category_data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CategoryData {
+  final String categoryName;
+  final String picturePath;
+  final Color color;
+
+  CategoryData( {required this.color,required this.categoryName, required this.picturePath});
+}
---
+++ lib/home/home_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/home/category_data.dart';
+import 'package:news_app/utils/app_colors.dart';
+import 'package:news_app/utils/images.dart';
+import 'widgets/left_category_item.dart';
+import 'widgets/right_category_item.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  static const String routeName = 'HomeScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: MediaQuery.of(context).size.height * .09,
+        leading: Icon(
+          color: AppColors.white,
+          Icons.menu,
+          size: 35,
+        ),
+        centerTitle: true,
+        title: Text(
+          'News App',
+          style: GoogleFonts.exo(
+            textStyle: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Pick your category',
+              style: GoogleFonts.poppins(
+                  textStyle: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: AppColors.darkGray, fontWeight: FontWeight.bold)),
+            ),
+            Text(
+              'of interest',
+              style: GoogleFonts.poppins(
+                  textStyle: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: AppColors.darkGray, fontWeight: FontWeight.bold)),
+            ),
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  LeftCategoryItem(
+                    category: CategoryData(
+                        categoryName: 'Sports', picturePath: ImagesPath.ball, color: AppColors.red),
+                  ),
+                  RightCategoryItem(
+                    category: CategoryData(
+                        categoryName: 'Politics',
+                        picturePath: ImagesPath.politics, color: AppColors.darkBlue),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  LeftCategoryItem(
+                    category: CategoryData(
+                        categoryName: 'Health', picturePath: ImagesPath.health, color: AppColors.pink),
+                  ),
+                  RightCategoryItem(
+                    category: CategoryData(
+                        categoryName: 'Bussines',
+                        picturePath: ImagesPath.bussines, color: AppColors.orange),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  LeftCategoryItem(
+                    category: CategoryData(
+                        categoryName: 'Environment',
+                        picturePath: ImagesPath.environment, color: AppColors.lightBlue),
+                  ),
+                  RightCategoryItem(
+                    category: CategoryData(
+                        categoryName: 'Science',
+                        picturePath: ImagesPath.science, color: AppColors.yellow),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
